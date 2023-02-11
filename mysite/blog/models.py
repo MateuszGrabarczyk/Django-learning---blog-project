e2ccabfa8e2237909c1f3944000094f4f6f9bed1
Mateusz Grabarczyk <mateusz.grabarczyk2001@gmail.com>
2023-02-11 08:10:56 +0100

    Added views to my blog app

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
 
 class Post(models.Model):
     STATUS_CHOICES = (
@@ -18,9 +21,18 @@ class Post(models.Model):
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
 
+    objects = models.Manager()
+    published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.publish.year, self.publish.strftime('%m'),
+                                                self.publish.strftime('%d'), self.slug])
+    
+
     class Meta:
         ordering = ('-publish',)
 
         def __str__(self):
             return self.title
+
 
