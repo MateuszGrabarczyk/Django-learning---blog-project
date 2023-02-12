fc14c3be91122757f3973b76dfafacca34409b8e
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-02-12 08:28:23 +0100

    build a form to share blog post by email 📧

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
+from django.core.mail import send_mail
 def post_list(request):
     object_list = Post.published.all()
     paginator = Paginator(object_list, 2)
@@ -26,3 +28,20 @@ def post_detail(request, year, month, day, post):
     publish__year=year, publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
     
+def post_share(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status='published')
+    sent = False
+
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cleaned_data = form.cleaned_data
+            post_url = request.build_absolute_uri(post.get_absolute_url())
+            subject = f'{cleaned_data["name"]} encourages to read "{post.title}"'
+            message = 'Read this post pleaseeeeee'
+            send_mail(subject, message, 'admin@myblog.com', [cleaned_data["to"]])
+            sent = True
+    else:
+        form = EmailPostForm()
+    return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+
