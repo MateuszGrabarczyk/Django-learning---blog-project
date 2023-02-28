64eee6013d8aa632223e18fdf401d5412460251a
Mateusz Grabarczyk <72306674+MateuszGrabarczyk@users.noreply.github.com>
2023-02-28 09:16:38 +0100

    Delete feedback/feedback directory

--- feedback/feedback/profiles/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls.static import static
-from . import views
-from django.conf import settings
-
-urlpatterns = [
-    path("", views.CreateProfileView.as_view()),
-    path("list", views.ProfilesView.as_view())
-] 
