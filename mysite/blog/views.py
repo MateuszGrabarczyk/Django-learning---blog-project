9d6021341a7c005b5fa18a578673202b2b4474e0
MateuszGrabarczyk <mategra240@student.polsl.pl>
2023-03-06 16:55:43 +0100

    Added tags to posts

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,8 +4,17 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from django.core.mail import send_mail
-def post_list(request):
+from taggit.models import Tag
+
+
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 2)
     page = request.GET.get('page')
     try:
@@ -15,7 +24,7 @@ def post_list(request):
     except EmptyPage:
         posts = paginator.page(paginator.num_pages)
 
-    return render(request, 'blog/post/list.html',{'posts': posts, 'page': page})
+    return render(request, 'blog/post/list.html',{'posts': posts, 'page': page, 'tag': tag})
 
 class PostListView(ListView):
     queryset = Post.published.all()
